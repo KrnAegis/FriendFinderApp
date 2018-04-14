5397672449fbba1eb562880173008eb013f43d04
KrnAegis <hyungthekoren@yahoo.com>
2018-04-14 15:52:26 -0400

    Picture updates

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+app.use(express.static('app/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
-
 
 
 
